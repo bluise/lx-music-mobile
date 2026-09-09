4ca7b7aba7c68289cbad9b1ee1a33d2d5c2b62ec
bluise <bluise@users.noreply.github.com>
2026-09-09 06:11:56 +0000

    feat: 生成项目 Code Wiki 文档
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,14 @@
-import { useEffect } from 'react'
+import { useEffect, useCallback } from 'react'
 import { useHorizontalMode } from '@/utils/hooks'
 import PageContent from '@/components/PageContent'
-import { setComponentId } from '@/core/common'
+import { setComponentId, setNavActiveId } from '@/core/common'
 import { COMPONENT_IDS } from '@/config/constant'
 import Vertical from './Vertical'
 import Horizontal from './Horizontal'
 import { navigations } from '@/navigation'
 import settingState from '@/store/setting/state'
+import { useBackHandler } from '@/utils/hooks/useBackHandler'
+import commonState from '@/store/common/state'
 
 
 interface Props {
@@ -16,6 +18,18 @@ interface Props {
 
 export default ({ componentId }: Props) => {
   const isHorizontalMode = useHorizontalMode()
+
+  // 当导航栈已到根（只有 Home）且不在歌单页/设置页时，按返回跳转到歌单页
+  useBackHandler(useCallback(() => {
+    if (Object.keys(commonState.componentIds).length == 1) {
+      if (commonState.navActiveId != 'nav_songlist' && commonState.navActiveId != 'nav_setting') {
+        setNavActiveId('nav_songlist')
+        return true
+      }
+    }
+    return false
+  }, []))
+
   useEffect(() => {
     setComponentId(COMPONENT_IDS.home, componentId)
     // eslint-disable-next-line react-hooks/exhaustive-deps
