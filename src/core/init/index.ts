af03f096c8bd3b6ef2817295d3717a52f713a373
bluise <bluise@users.noreply.github.com>
2026-09-09 04:38:27 +0000

    fix: remove update check + hide Version and About from settings

--- src/core/init/index.ts
+++ src/core/init/index.ts
@@ -11,7 +11,6 @@ import { initDeeplink } from './deeplink'
 import { setApiSource } from '@/core/apiSource'
 import commonActions from '@/store/common/action'
 import settingState from '@/store/setting/state'
-import { checkUpdate } from '@/core/version'
 import { bootLog } from '@/utils/bootLog'
 import { cheatTip } from '@/utils/tools'
 import { BUILTIN_USER_API_ID } from '@/resources/userApi/builtin'
@@ -22,7 +21,6 @@ const handlePushedHomeScreen = async() => {
   if (settingState.setting['common.isAgreePact']) {
     if (isFirstPush) {
       isFirstPush = false
-      void checkUpdate()
       void initDeeplink()
     }
   } else {
